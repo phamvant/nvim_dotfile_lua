7602f9b712377a0d6e6b2522f22bf1472c9258cc
phamvant <pham.t.286.01@gmail.com>
2022-12-27 08:22:22 +0700

    nvim-tree get into folder of the file searched by telescope

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,6 +1,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  sort_by = "extension",
+})
 
 vim.keymap.set('n', '<c-e>', ':NvimTreeFindFileToggle<CR>')
--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,7 +1,44 @@
 require('telescope').setup()
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<c-p>', builtin.find_files, {}) 
+vim.keymap.set('n', '<c-p>', builtin.find_files, {})
 vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+
+local open_in_nvim_tree = function(prompt_bufnr)
+    local action_state = require "telescope.actions.state"
+    local Path = require "plenary.path"
+    local actions = require "telescope.actions"
+
+    local entry = action_state.get_selected_entry()[1]
+    local entry_path = Path:new(entry):parent():absolute()
+    actions._close(prompt_bufnr, true)
+    entry_path = Path:new(entry):parent():absolute()
+    entry_path = entry_path:gsub("\\", "\\\\")
+
+    vim.cmd("NvimTreeClose")
+    vim.cmd("NvimTreeOpen " .. entry_path)
+
+    local file_name = nil
+    for s in string.gmatch(entry, "[^/]+") do
+        file_name = s
+    end
+
+    vim.cmd("/" .. file_name)
+end
+
+local telescope = require('telescope')
+
+telescope.setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<CR>"] = open_in_nvim_tree,
+            },
+            n = {
+                ["<CR>"] = open_in_nvim_tree,
+            },
+        },
+    },
+}
